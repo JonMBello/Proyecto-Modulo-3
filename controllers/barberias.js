c4db16b00e9cc3d872cc43624d158032a841bb4e
Jordan099 <jordan-gallardo@hotmail.com>
2021-03-14 20:56:41 -0600

    Añadiendo funciones

--- controllers/barberias.js
+++ controllers/barberias.js
@@ -4,28 +4,75 @@ const mongoose = require('mongoose')
 const Barberia = mongoose.model('Barberia')
 
 
-
 function crearBarberia(req, res, next) {
   var barberia = new Barberia(req.body)
-  Barberia.save().then(barberia => {
+  barberia.save().then(barberia => {
     res.status(201).send(barberia)
   }).catch(next)
 }
-
-/*function obtenerMascotas(req, res) {
-  // Simulando dos Mascotas y respondiendolos
-  var mascota1 = new Mascota(1, 'Nochipa', 'Perro', 'https://www.perrosrazapequeña.com/wp-content/uploads/2018/06/chihuahua-pelo-largo.jpg','bien bonita','1','CDMX');
-  var mascota2 = new Mascota(1, 'Tito', 'Tortuga', 'https://img.culturacolectiva.com/featured/2019/02/27/1551305058738/tortugas-japonesas-se-vuelven-plaga-en-mexico-high.png','verde','1','CDMX');
-  res.send([mascota1, mascota2])
-}*/ 
 
 function obtenerBarberias(req, res, next) {
   if(req.params.id){
     Barberia.findById(req.params.id).then(barberia => {
 	      res.send(barberia)
 	    }).catch(next)
+      
   } else {
     Barberia.find().then(barberia=>{
+      res.send(barberia)
+    }).catch(next)
+  }
+}
+
+function obtenerBarberiasPorLimite(req, res, next){
+  let limite = req.params.limit;
+  limite = Number(limite)
+  Barberia.find({}).limit(limite).then(barberia => {
+    res.send(barberia)
+  }).catch(next)
+}
+
+function obtenerBarberiasPorCampos(req, res, next){
+  let datos = ''
+  for(const dato in req.query){
+    if(Object.hasOwnProperty.call(req.query, dato)){
+      datos = datos + ' ' + req.query[dato];
+    }
+  }
+  Barberia.find({}, datos).then(barberia =>{
+    res.send(barberia)
+  }).catch(next)
+}
+
+function obtenerBarberiasPorAtributo(req,res,next){
+  if(req.params.nombre){
+    Barberia.find({nombre: req.params.nombre}).then(barberia =>{
+      res.send(barberia)
+    }).catch(next)
+  }
+  if(req.params.direccion){
+    Barberia.find({direccion: req.params.direccion}).then(barberia =>{
+      res.send(barberia)
+    }).catch(next)
+  }
+  if(req.params.telefono){
+    console.log(req.params.telefono)
+    Barberia.find({telefono: req.body.telefono}).then(barberia =>{
+      res.send(barberia)
+    }).catch(next)
+  }
+  if(req.params.correo){
+    Barberia.find({correo: req.params.correo}).then(barberia =>{
+      res.send(barberia)
+    }).catch(next)
+  }
+  if(req.params.horario){
+    Barberia.find({horario: req.params.horario}).then(barberia =>{
+      res.send(barberia)
+    }).catch(next)
+  }
+  if(req.params.barberoEncargado){
+    Barberia.find({horario: req.params.barberoEncargado}).then(barberia =>{
       res.send(barberia)
     }).catch(next)
   }
@@ -35,50 +82,41 @@ function obtenerBarberias(req, res, next) {
 
 function modificarBarberia(req, res, next) {
   console.log(req.params.id)
-  Usuario.findById(req.params.id).then(barberia => {
+  Barberia.findById(req.params.id).then(barberia => {
     if (!barberia) { return res.sendStatus(401); }
     let nuevaInfo = req.body
     if (typeof nuevaInfo.nombre !== 'undefined')
       barberia.nombre = nuevaInfo.nombre
     if (typeof nuevaInfo.direccion !== 'undefined')
       barberia.direccion = nuevaInfo.direccion
-    if (typeof nuevaInfo.foto !== 'undefined')
-      barberia.telefono = nuevaInfo.telefono
-    if (typeof nuevaInfo.ubicacion !== 'undefined')
-      barberia.correo = nuevaInfo.correo
     if (typeof nuevaInfo.telefono !== 'undefined')
+      barberia.telefono = nuevaInfo.telefono
+    if (typeof nuevaInfo.correo !== 'undefined')
+      barberia.correo = nuevaInfo.correo
+    if (typeof nuevaInfo.horario !== 'undefined')
       barberia.horario = nuevaInfo.horario
       if (typeof nuevaInfo.barberoEncargado !== 'undefined')
       barberia.barberoEncargado = nuevaInfo.barberoEncargado
-    barberia.save().then(barberiaActualizada => {                                   //Guardando usuario modificado en MongoDB.
+    barberia.save().then(barberiaActualizada => {                                   //Guardando barberia modificada en MongoDB.
       res.status(201).json(barberiaActualizada.publicData())
     }).catch(next)
   }).catch(next)
 }
 
-/*function eliminarMascota(req, res) {
-    // Líneas que buscan un usaurio en la bd, una vez que lo encuenra lo elimina.
-  res.status(200).send(`Mascota ${req.params.id} eliminado`);
-}*/
-
 
   function eliminarBarberia(req, res) {
-    // únicamente borra a su propio usuario obteniendo el id del token
-    Usuario.findOneAndDelete({ _id: req.usuario.id }).then(r => {         //Buscando y eliminando usuario en MongoDB.
-      res.status(200).send(`Usuario ${req.params.id} eliminado: ${r}`);
+    Barberia.findOneAndDelete({ _id: req.params.id }).then(r => {         //Buscando y eliminando barberua en MongoDB.
+      res.status(200).send(`Barberia ${req.params.id} eliminado: ${r.nombre}`);
     })
   }
-
-  
-  /*Mascota.findOneAndDelete({ _id: req.param.id }).then(r => {
-      res.status(200).send(`Mascota ${req.params.id} eliminada: ${r}`);
-  });*/
-}
 
 
 module.exports = {
   crearBarberia,
   obtenerBarberias,
+  obtenerBarberiasPorLimite,
+  obtenerBarberiasPorCampos,
+  obtenerBarberiasPorAtributo,
   modificarBarberia,
   eliminarBarberia,
 }
--- routes/barberias.js
+++ routes/barberias.js
@@ -7,12 +7,24 @@ const {
   crearBarberia,
   obtenerBarberias,
   modificarBarberia,
-  eliminarBarberia
+  eliminarBarberia,
+  obtenerBarberiasPorAtributo,
+  obtenerBarberiasPorCampos,
+  obtenerBarberiasPorLimite
 } = require('../controllers/barberias')
 
 
 //Se define el metodo http que tendra cada uno de nuestros metodos
 router.get('/', obtenerBarberias)
+router.get('/:id', obtenerBarberias)
+router.get('/limite/:limit', obtenerBarberiasPorLimite)
+router.get('/buscarCampos?nombre',obtenerBarberiasPorCampos)
+router.get('/buscarNombre/:nombre/:direccion',obtenerBarberiasPorAtributo)
+router.get('/buscarDireccion/:direccion',obtenerBarberiasPorAtributo)
+router.get('/buscarTelefono/:telefono',obtenerBarberiasPorAtributo)
+router.get('/buscarCorreo/:correo',obtenerBarberiasPorAtributo)
+router.get('/buscarHorario/:horario',obtenerBarberiasPorAtributo)
+router.get('/buscarEncargado/:barberoEncargado',obtenerBarberiasPorAtributo)
 router.post('/', crearBarberia)
 router.put('/:id', modificarBarberia)
 router.delete('/:id', eliminarBarberia)
